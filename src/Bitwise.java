dff3180c4385ffb0672ba4b6f4f935fdf31b1888
Joseph Vanderzwart <josephvanderzwart@protonmail.com>
2025-07-04 14:27:14 -0600

    second

--- src/Bitwise.java
+++ src/Bitwise.java
@@ -1,2 +1,39 @@
+import java.awt.image.BufferedImage;
+import javax.imageio.ImageIO;
+import java.io.File;
+import java.nio.Buffer;
+
+
 public class Bitwise {
+
+    private Bitwise()
+    {
+
+    }
+
+    public static void bit(File inFile, File targetFile)
+    {
+        System.out.println("~~ bitwise working bit...");
+        BufferedImage after = null;
+        BufferedImage before = null;
+        try
+        {
+            before = ImageIO.read(inFile);
+        }
+        catch (Exception e)
+        {
+            System.out.println("FUCK !!!! ITS ALL GONE WRONG !!!!");
+            e.printStackTrace();
+        }
+        after = new BufferedImage(before.getWidth(), before.getHeight(), BufferedImage.TYPE_INT_RGB);
+        try
+        {
+            ImageIO.write(after, "png", targetFile);
+        }
+        catch (Exception e)
+        {
+            System.out.println("YOU DIRTY BASTARD!  HE'S DONE IT AGAIN!");
+            e.printStackTrace();
+        }
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,48 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.io.File;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello and welcome!");
+        boolean running = true;
+        Scanner scanner = new Scanner(System.in);
+        String outputFolder = "/home/joe/Pictures/bitwise";
+        File inFile = null;
+        File outFile = null;
 
+        System.out.println("~~Bitwise Image Editor~~");
+        while (running) {
+            System.out.print("enter command:");
+            String input = scanner.nextLine();
+
+            switch (input) {
+                case "q":
+                    running = false;
+                    System.out.println("quitting.");
+                    break;
+                case "load":
+                    System.out.print(">> new file name? ");
+                    String targetName = scanner.nextLine();
+                    String path = "/home/joe/Pictures/memes/us.png";
+                    System.out.println(">> input file:" + path);
+                    inFile = new File(path);
+                    outFile = new File(outputFolder + "/" + targetName + ".png");
+                    System.out.println(">> input file exists: " + inFile.exists());
+                    try
+                    {
+                        System.out.println(">> output file create: " + outFile.createNewFile());
+                    }
+                    catch (Exception e)
+                    {
+                        e.printStackTrace();
+                    }
+                    System.out.println(">> file load complete.");
+                    break;
+                case "bit":
+                    System.out.println(">>bit");
+                    Bitwise.bit(inFile, outFile);
+                    break;
+            }
         }
+
     }
+}
